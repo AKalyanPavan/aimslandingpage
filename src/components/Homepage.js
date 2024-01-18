10dd4a18019b4d8b7ac0f5d8b7e927d14fd2c367
A Kalyan Pavan <pavanaravelli690@gmail.com>
2024-01-18 15:15:41 -0500

    adjusted borders and box shadows

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -133,8 +133,8 @@ function Homepage() {
 				<div id="aboutus" className="text-center">
 					<div className="text-[25px] font-bold">About AIMS</div>
 					<div className="text-[#677788] text-[20px] leading-[32px] my-[50px] max-w-[1000px] mx-auto">AIMS specializes in nurturing early-stage and expanding businesses. We're sector-agnostic experts passionate about transforming ideas into successful enterprises.</div>
-					<div className="min-[1000px]:flex justify-between">
-						<div className="w-[300px] border-[#67778870] border-[1px] border-solid rounded-[10px] p-[20px] max-[1000px]:mx-[auto]">
+					<div className="min-[1101px]:flex justify-between">
+						<div className="w-[400px] rounded-[10px] p-[20px] max-[1101px]:mx-[auto]">
 							<div className="flex justify-center">
 								<svg width="60" height="60" viewBox="0 0 61 60" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<rect x="0.330017" width="60" height="60" rx="30" fill="#377DFF" fill-opacity="0.1"/>
@@ -142,28 +142,28 @@ function Homepage() {
 									<path d="M29.5 24.8182C26.8249 24.8182 24.6562 27.362 24.6562 30.5C24.6562 33.638 26.8249 36.1818 29.5 36.1818C32.1751 36.1818 34.3438 33.638 34.3438 30.5C34.3438 27.362 32.1751 24.8182 29.5 24.8182ZM22.7188 30.5C22.7188 26.1068 25.7548 22.5455 29.5 22.5455C33.2452 22.5455 36.2812 26.1068 36.2812 30.5C36.2812 34.8932 33.2452 38.4545 29.5 38.4545C25.7548 38.4545 22.7188 34.8932 22.7188 30.5Z" fill="#377DFF"/>
 								</svg>
 							</div>
-							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] my-[40px]">Building your vision together</div>
-							<div className="text-[16px] text-[#677788]">AIMS specializes in nurturing early-stage and expanding businesses. We're sector-agnostic experts passionate about transforming ideas into successful enterprises.</div>
+							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] mt-[40px]">Building your vision together</div>
+							<div className="text-[16px] text-[#677788] mt-[40px]">AIMS specializes in nurturing early-stage and expanding businesses. We're sector-agnostic experts passionate about transforming ideas into successful enterprises.</div>
 						</div>
-						<div className="w-[300px] max-[999px]:my-[30px] min-[1000px]:mx-[20px] border-[#67778870] border-[1px] border-solid rounded-[10px] p-[20px] max-[1000px]:mx-[auto]">
+						<div className="w-[400px] max-[999px]:my-[30px] min-[1101px]:mx-[20px] rounded-[10px] p-[20px] max-[1101px]:mx-[auto]">
 							<div className="flex justify-center">
 								<svg width="60" height="60" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<rect width="60" height="60" rx="30" fill="#377DFF" fill-opacity="0.1"/>
 									<path d="M25 39C23.9391 39 22.9217 38.5786 22.1716 37.8284C21.4214 37.0783 21 36.0609 21 35V23C21 22.4696 21.2107 21.9609 21.5858 21.5858C21.9609 21.2107 22.4696 21 23 21H27C27.5304 21 28.0391 21.2107 28.4142 21.5858C28.7893 21.9609 29 22.4696 29 23V35C29 36.0609 28.5786 37.0783 27.8284 37.8284M25 39C26.0609 39 27.0783 38.5786 27.8284 37.8284M25 39H37C37.5304 39 38.0391 38.7893 38.4142 38.4142C38.7893 38.0391 39 37.5304 39 37V33C39 32.4696 38.7893 31.9609 38.4142 31.5858C38.0391 31.2107 37.5304 31 37 31H34.657M27.8284 37.8284L36.314 29.343C36.6889 28.9679 36.8996 28.4593 36.8996 27.929C36.8996 27.3987 36.6889 26.8901 36.314 26.515L33.485 23.686C33.1099 23.3111 32.6013 23.1004 32.071 23.1004C31.5407 23.1004 31.0321 23.3111 30.657 23.686L29 25.343M25 35H25.01" stroke="#377DFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 								</svg>
 							</div>
-							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] my-[40px]">Our Approach</div>
-							<div className="text-[16px] text-[#677788]"> From setting up your initial team to strategic marketing and financial planning, AIMS is dedicated to ensuring your startup achieves product-market fit and robust early revenue growth.</div>
+							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] mt-[40px]">Our Approach</div>
+							<div className="text-[16px] text-[#677788] mt-[40px]"> From setting up your initial team to strategic marketing and financial planning, AIMS is dedicated to ensuring your startup achieves product-market fit and robust early revenue growth.</div>
 						</div>
-						<div className="w-[300px] border-[#67778870] border-[1px] border-solid rounded-[10px] p-[20px] max-[1000px]:mx-[auto]">
+						<div className="w-[400px] rounded-[10px] p-[20px] max-[1101px]:mx-[auto]">
 							<div className="flex justify-center">
 								<svg width="60" height="63" viewBox="0 0 60 63" fill="none" xmlns="http://www.w3.org/2000/svg">
 									<rect width="60" height="62.9893" rx="30" fill="#377DFF" fill-opacity="0.1"/>
 									<path d="M28 39L32 23M36 27L40 31L36 35M24 35L20 31L24 27" stroke="#377DFF" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 								</svg>
 							</div>
-							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] my-[40px]">Sector Agnostic Expertise</div>
-							<div className="text-[16px] text-[#677788]">AIMS is adept across various sectors, including Fintech, Sportstech, MedTech, Edutech, Deeptech, and more. This broad expertise enables us to provide unique insights and tailored solutions for each startup, regardless of their industry.</div>
+							<div className="text-[18px] font-semibold leading-[28px] text-[#1E2022] mt-[40px]">Sector Agnostic Expertise</div>
+							<div className="text-[16px] text-[#677788] mt-[40px]">AIMS is adept across various sectors, including Fintech, Sportstech, MedTech, Edutech, Deeptech, and more. This broad expertise enables us to provide unique insights and tailored solutions for each startup, regardless of their industry.</div>
 						</div>
 					</div>
 				</div>
@@ -172,32 +172,32 @@ function Homepage() {
 				<div id="ourservices" className="text-center">
 					<div className="text-[32px] font-bold  my-[50px]">Our Services</div>
 					<div className="grid min-[1100px]:grid-cols-3 min-[700px]:grid-cols-2 grid-cols-1 gap-[20px]">
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={growth} alt="Growth"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Strategic Direction</div>
 							<div className="text-[#677788]">Crafting a clear path for your business, focusing on market fit and sustainable growth.</div>
 						</div>
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={team} alt="Team"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Team Building and Operation</div>
 							<div className="text-[#677788]">Assembling a dynamic team and streamlining operations to maximize efficiency and innovation.</div>
 						</div>
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={networking} alt="Networking"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Marketing and Outreach</div>
 							<div className="text-[#677788]">Creating impactful marketing strategies that resonate with your target audience and amplify your reach.</div>
 						</div>
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={capital} alt="Capital"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Capital Planning</div>
 							<div className="text-[#677788]">Navigating the complexities of funding, from equity and debt to alternative financial solutions.</div>
 						</div>
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={research} alt="Research"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]">Market Research & Analysis</div>
 							<div className="text-[#677788]">Utilizing cutting-edge tools and analytics to understand market trends, customer needs, and competitive landscapes.</div>
 						</div>
-						<div className="shadow-[1px_4px_5px_1px_rgba(0,0,0,0.3)] p-[20px] text-left rounded-[10px]">
+						<div className="shadow-[1px_1px_3px_1px_rgba(0,0,0,0.05)] p-[20px] text-left rounded-[10px]">
 							<img src={business} alt="Business"/>
 							<div className="text-[20px] text-[#1E2022] font-medium my-[30px]"> Business Scaling Strategies</div>
 							<div className="text-[#677788]">Developing scalable business models and strategies to facilitate sustainable growth and expansion.</div>
@@ -257,34 +257,3 @@ function Homepage() {
 }
 
 export default Homepage;
-
-
-// https://themes.getbootstrap.com/preview/?theme_id=4231
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
